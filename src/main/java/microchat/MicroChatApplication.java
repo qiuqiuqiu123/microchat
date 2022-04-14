106dfc7348fca39accf7b5cf07beec6d37b03d7d
ky <1905945218@qq.com>
2022-04-15 01:13:35 +0800

    2022/4/15 加入eureka服务注册中心

--- src/main/java/microchat/MicroChatApplication.java
+++ src/main/java/microchat/MicroChatApplication.java
@@ -3,9 +3,11 @@ package microchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableEurekaClient
 public class MicroChatApplication {
 
     public static void main(String[] args) {
